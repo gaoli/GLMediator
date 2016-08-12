bb60c275b048875735004774f22454020d6eb97d
高力 <3071730@qq.com>
2016-08-12 16:13:54 +0800

    添加 GLMediator

---
+++ GLMediator/Classes/GLMediator.h
@@ -0,0 +1,13 @@
+//
+//  GLMediator.h
+//  Pods
+//
+//  Created by gaoli on 16/8/12.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GLMediator : NSObject
+
+@end
---
+++ GLMediator/Classes/GLMediator.m
@@ -0,0 +1,13 @@
+//
+//  GLMediator.m
+//  Pods
+//
+//  Created by gaoli on 16/8/12.
+//
+//
+
+#import "GLMediator.h"
+
+@implementation GLMediator
+
+@end
