64fff5b7c06bdc2d62815ffb6a88efbe592afc8e
高力 <3071730@qq.com>
2016-08-16 16:57:08 +0800

    更新演示

---
+++ Example/GLMediator/Modules/ModuleA/GLModuleA.h
@@ -0,0 +1,16 @@
+//
+//  GLModuleA.h
+//  GLMediator
+//
+//  Created by gaoli on 16/8/16.
+//  Copyright © 2016年 gaoli. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GLModuleA : NSObject
+
+- (NSString *)actionA;
+- (NSString *)actionB:(NSString *)name;
+
+@end
---
+++ Example/GLMediator/Modules/ModuleA/GLModuleA.m
@@ -0,0 +1,21 @@
+//
+//  GLModuleA.m
+//  GLMediator
+//
+//  Created by gaoli on 16/8/16.
+//  Copyright © 2016年 gaoli. All rights reserved.
+//
+
+#import "GLModuleA.h"
+
+@implementation GLModuleA
+
+- (NSString *)actionA {
+    return @"hello, world";
+}
+
+- (NSString *)actionB:(NSDictionary *)params {
+    return [NSString stringWithFormat:@"hello, %@", params[@"name"]];
+}
+
+@end
